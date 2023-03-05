27348008d215b5f4642fd5ade4a08598b8a6976f
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-03-05 19:40:41 +0000

    add App component with relative files & reset.css

---
+++ src/Components/App.js/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span class="highlight">mmm</span>ing
+        </h1>
+        <div class="App">
+          {/* <SearchBar/> */}
+          <div class="App-playlist">
+            {/* <SearchResults/> */}
+            {/* <Playlist/> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
